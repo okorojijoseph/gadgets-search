eb776584af4854bb8e83484d2d5f44f9d6d0e756
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2024-03-20 16:18:24 +0100

    chore: updated login

--- shop/models.py
+++ shop/models.py
@@ -21,7 +21,7 @@ class BaseModel(models.Model):
 
 
 class SiteDetail(BaseModel):
-    name = models.CharField(max_length=200, default="Gadgets Lookup")
+    name = models.CharField(max_length=200, default="ShopSmart")
     desc = models.TextField(
         default="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     )
